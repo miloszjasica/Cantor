558cde243dc1c5a21e8b60ae525dc627cfa2366e
Milosz <miloszjasica1@gmail.com>
2026-02-17 12:24:11 +0100

    Initial commit

---
+++ src/main/java/com/milosz/cantor/CantorApplication.java
@@ -0,0 +1,13 @@
+package com.milosz.cantor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milosz/cantor/CantorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milosz.cantor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
